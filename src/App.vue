1f0ebf06c2639584ef314dc921d99c8f2c15f7f9
xdenote <xdenote@gmail.com>
2023-12-28 14:29:48 +0500

    Total Calculation

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,34 @@ import Balance from "@/components/Balance.vue";
 import IncomeExpenses from "@/components/IncomeExpenses.vue";
 import TransactionList from "@/components/TransactionList.vue";
 import AddTransactions from "@/components/AddTransactions.vue";
-import { ref } from "vue";
+import {ref, computed} from "vue";
 
 const transactions = ref([
-  { id: 1, text: "Flower", amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 },
+  {id: 1, text: "Flower", amount: -20},
+  {id: 2, text: "Salary", amount: 300},
+  {id: 3, text: "Book", amount: -10},
+  {id: 4, text: "Camera", amount: 150},
 ]);
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => (acc += transaction.amount), 0).toFixed(2);
+});
+
+const income = computed(() => {
+  return transactions.value.filter((transaction) => transaction.amount > 0).reduce((acc, transaction) => (acc += transaction.amount), 0).toFixed(2);
+});
+
+const expense = computed(() => {
+  return transactions.value.filter((transaction) => transaction.amount < 0).reduce((acc, transaction) => (acc += transaction.amount), 0).toFixed(2);
+});
 
 </script>
 
 <template>
   <Header/>
   <div class="container">
-    <Balance/>
-    <IncomeExpenses/>
+    <Balance :total="total"/>
+    <IncomeExpenses :income="income" :expense="expense"/>
     <TransactionList :transactions="transactions"/>
     <AddTransactions/>
   </div>
--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,10 +1,18 @@
 <script setup>
+import {  defineProps } from 'vue'
+
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true
+  }
+})
 
 </script>
 
 <template>
   <h4>Your Balance</h4>
-  <h1 id="balance">$0.00</h1>
+  <h1 id="balance">${{total}}</h1>
 </template>
 
 <style scoped>
