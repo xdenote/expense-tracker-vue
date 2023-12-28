cd8509b8e176d79665d935345cfd4ba0f60a524b
xdenote <xdenote@gmail.com>
2023-12-28 14:17:39 +0500

    Global State

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@ import Balance from "@/components/Balance.vue";
 import IncomeExpenses from "@/components/IncomeExpenses.vue";
 import TransactionList from "@/components/TransactionList.vue";
 import AddTransactions from "@/components/AddTransactions.vue";
+import { ref } from "vue";
+
+const transactions = ref([
+  { id: 1, text: "Flower", amount: -20 },
+  { id: 2, text: "Salary", amount: 300 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 },
+]);
 
 </script>
 
@@ -12,7 +20,7 @@ import AddTransactions from "@/components/AddTransactions.vue";
   <div class="container">
     <Balance/>
     <IncomeExpenses/>
-    <TransactionList/>
+    <TransactionList :transactions="transactions"/>
     <AddTransactions/>
   </div>
 </template>
--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -3,16 +3,7 @@
 </script>
 
 <template>
-  <div class="inc-exp-container">
-    <div>
-      <h4>Income</h4>
-      <p id="money-plus" class="money plus">+$0.00</p>
-    </div>
-    <div>
-      <h4>Expense</h4>
-      <p id="money-minus" class="money minus">-$0.00</p>
-    </div>
-  </div>
+
 </template>
 
 <style scoped>
