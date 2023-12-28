5e61057a5b3dd489fc2250edc1eba467b1418a9d
xdenote <xdenote@gmail.com>
2023-12-28 14:12:33 +0500

    Transaction Display List

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
-<script>
+<script setup>
 import Header from "@/components/Header.vue";
-import {defineComponent} from "vue";
 import Balance from "@/components/Balance.vue";
 import IncomeExpenses from "@/components/IncomeExpenses.vue";
 import TransactionList from "@/components/TransactionList.vue";
 import AddTransactions from "@/components/AddTransactions.vue";
-
-export default defineComponent({
-  components: {AddTransactions, TransactionList, IncomeExpenses, Balance, Header}
-})
-
 
 </script>
 
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,19 +1,25 @@
 <script setup>
+const transactions = [
+  {id: 1, text: 'Flower', amount: -20},
+  {id: 2, text: 'Salary', amount: 300},
+  {id: 3, text: 'Book', amount: -10},
+  {id: 4, text: 'Camera', amount: 150},
+]
 
 </script>
 
 <template>
   <h3>History</h3>
+
   <ul id="list" class="list">
-    <li class="minus">
-      Cash <span>-$400</span><button class="delete-btn">x</button>
-    </li>
-    <li class="plus">
-      Salary <span>-$500</span><button class="delete-btn">x</button>
+    <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+      {{ transaction.text }} <span>${{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
 
 <style scoped>
+
 
 </style>
