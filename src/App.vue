8631dd6dc2967ee48a7e7ca8d9be4cbd0427a479
xdenote <xdenote@gmail.com>
2023-12-29 13:00:03 +0500

    Completed the App

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,19 @@ import Balance from "@/components/Balance.vue";
 import IncomeExpenses from "@/components/IncomeExpenses.vue";
 import TransactionList from "@/components/TransactionList.vue";
 import AddTransactions from "@/components/AddTransactions.vue";
-import {ref, computed} from "vue";
+import {ref, computed, onMounted} from "vue";
 import {useToast} from "vue-toastification";
 
 const toast = useToast();
 
-const transactions = ref([
-  {id: 1, text: "Flower", amount: -20},
-  {id: 2, text: "Salary", amount: 300},
-  {id: 3, text: "Book", amount: -10},
-  {id: 4, text: "Camera", amount: 150},
-]);
+const transactions = ref([]);
+
+onMounted(async () => {
+  const savedTransactions = JSON.parse(localStorage.getItem("transactions"));
+  if(savedTransactions) {
+    transactions.value = savedTransactions;
+  }
+});
 
 const total = computed(() => {
   return transactions.value.reduce((acc, transaction) => (acc += transaction.amount), 0).toFixed(2);
@@ -31,13 +33,20 @@ const expense = computed(() => {
 const handleTransaction = (transaction) => {
   transactions.value.push(transaction);
   toast.success("Transaction added successfully");
+  saveTransactions();
   console.log(transactions.value);
 };
 
 const deletedTransaction = (id) => {
   transactions.value = transactions.value.filter((transaction) => transaction.id !== id);
   toast.success("Transaction deleted successfully");
+  saveTransactions();
   console.log(transactions.value);
+};
+
+//save to local storage
+const saveTransactions = () => {
+  localStorage.setItem("transactions", JSON.stringify(transactions.value));
 };
 
 </script>
--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -17,11 +17,11 @@ const props = defineProps({
   <div class="inc-exp-container">
     <div>
       <h4>Income</h4>
-      <p id="money-plus" class="money plus">+${{ income }}</p>
+      <p id="money-plus" class="money plus">${{ income }}</p>
     </div>
     <div>
       <h4>Expense</h4>
-      <p id="money-minus" class="money minus">-${{ expense }}</p>
+      <p id="money-minus" class="money minus">${{ expense }}</p>
     </div>
   </div>
 </template>
